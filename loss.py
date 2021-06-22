a23cd962bde53950d075eebac036ba007a695ccb
CQUEEN-lpy <79646025+CQUEEN-lpy@users.noreply.github.com>
2021-06-22 16:00:49 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,19 +1,25 @@
 import tensorflow as tf
 from tensorflow import keras
 import random
+import numpy as np
 """
 this py define the loss function that compute the NL and PL
 cp_label means the complementary label(random generated)
 """
 class CustomLoss(keras.losses.Loss):
-    def __init__(self, class_num, name="custom_loss"):
+    def __init__(self, class_num, param=0.01, name="custom_loss"):
         super().__init__(name=name)
         self.class_num = class_num
         self.th = 1/self.class_num
         self.rd = random.random()
+        self.rd = 0
+        self.param = param
 
     def call(self, y_true, y_pred):
-        self.batch_size = y_true.shape[0]
+
+        self.batch_size = tf.shape(y_true)[0]
+        y_true = tf.reshape(y_true, [self.batch_size, 1])
+
         self.cp_label = tf.reshape(tf.random.categorical([[1.0]*self.class_num], self.batch_size), [self.batch_size,1])                                  #shape = (batch_size,1)
         self.cp_label_onehot = tf.reshape(tf.one_hot(self.cp_label, axis=1, depth=self.class_num), [self.batch_size, self.class_num])                    #shape = (batch_size, class_nums)
         self.y_true_onehot = tf.reshape(tf.one_hot(y_true, axis=1, depth=self.class_num), [self.batch_size, self.class_num])                        #shape = (batch_size, class_nums)
@@ -22,7 +28,7 @@ class CustomLoss(keras.losses.Loss):
 
         NL_score = self.NL(y_pred, self.cp_label)
         PL_score = self.PL(y_pred, self.predict_label)
-        score = NL_score + PL_score
+        score = NL_score + self.param * PL_score
 
         return score
 
@@ -50,53 +56,34 @@ class CustomLoss(keras.losses.Loss):
 
     def PL(self, y_pred, pred_label):
         # select the predict score that satisfied the th
-        D = None
-        p = None
-        for i in range(y_pred.shape[0]):
-            flag = True
-            for j in range(y_pred.shape[1]):
-                if j == pred_label[i]:
-                    p = y_pred[i][j]
-                    continue
-                if y_pred[i][j] > self.th:
-                    flag = False
-                    break
-            if flag and self.rd < p:
-                if D is None:
-                    D = tf.reshape(y_pred[i], [1, self.class_num])
-                else:
-                    D = tf.concat((D, tf.reshape(y_pred[i], [1, self.class_num])),
-                                  axis=0)
+        one = tf.ones_like(y_pred)
+        zero = tf.zeros_like(y_pred)
+        label = tf.where(y_pred < self.th, x=zero, y=one)                   #shape = (batch_size, class_num)
+        label = tf.reduce_sum(label, axis=-1)                               #shape = (batch_size)
 
-        # calculate the PL                                                  D's shape = (n, class_num)
-        if D is None:
-            return 0
-        index = tf.cast(tf.reshape(tf.linspace(0, D.shape[0] - 1,
-                                               D.shape[0]),
-                                   [D.shape[0], 1]),
+        one = tf.ones_like(label)
+        zero = tf.zeros_like(label)
+        label = tf.where(label < 2, x=one, y=zero)
+        D = y_pred * tf.reshape(label, [self.batch_size, 1])                #shape = (batch_size, class_num)
+
+        # calculate the PL
+        num = self.batch_size
+        index = tf.cast(tf.reshape(tf.linspace(0, num - 1,
+                                               num),
+                                   [num, 1]),
                         dtype='int64')                                      # shape = (n, 1)
         D_label = tf.reshape(tf.argmax(D, axis=1),
-                            [D.shape[0], 1])                                # shape = (n, 1)
+                            [num, 1])                                       # shape = (n, 1)
         index = tf.concat((index, D_label), axis=1)                         # shape = (n, 2)
         py = tf.gather_nd(D, index)                                         # shape = (1, n)
         py = 1 - tf.math.square(py)                                         # shape = (1, n)
-        py = tf.reshape(py,[D.shape[0]])                                    # shape = (n)
-        weight = py[0]
-        for i in range(1, py.shape[0]):
-            weight = weight * py[i]                                         # shape = (1)
+        py = tf.reshape(py, [1,num])                                          # shape = (n)
+        weight = tf.reduce_prod(py)
 
-        cross_entropy = self.y_true_onehot * tf.math.log(y_pred)            # shape = (batch_size, class_nums)
+        cross_entropy = self.y_true_onehot * tf.math.log(y_pred)             # shape = (batch_size, class_nums)
         cross_entropy = tf.reduce_sum(cross_entropy, axis=1)                # shape = (batch_size, 1)
 
         out = -weight * cross_entropy
         out = tf.reduce_sum(out, axis=0)
 
         return out
-
-
-
-
-
-
-
-
